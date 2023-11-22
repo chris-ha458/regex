ca585b9a818e56623d90054714c7ed1423966ead
chris-ha458 <hac541309@gmail.com>
2023-11-22 21:57:20 +0900

    one liners across many modules

--- regex-automata/src/nfa/thompson/compiler.rs
+++ regex-automata/src/nfa/thompson/compiler.rs
@@ -1663,7 +1663,7 @@ impl Compiler {
         capture_index: u32,
         name: Option<&str>,
     ) -> Result<StateID, BuildError> {
-        let name = name.map(|n| Arc::from(n));
+        let name = name.map(Arc::from);
         self.builder.borrow_mut().add_capture_start(
             StateID::ZERO,
             capture_index,
